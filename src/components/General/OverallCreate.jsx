be84a605ed4fb39c9a8ac2d228126a8c40962838
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-12 18:32:58 -0500

    Falta 5 para terminar

--- src/components/General/OverallCreate.jsx
+++ src/components/General/OverallCreate.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { overallCreate } from '../hooks/useCRUD';
 import "./OverallCreate.scss";
+import {Form, Button, Select} from 'semantic-ui-react';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 import { httpConToken } from "../../helpers/http";
-import DinamicField from '../Root/procesos/MaestrosMatrix/DinamicField/DinamicField';
 
-function OverallCreate(props) {
-    const [state, setState] = React.useState({});
-
-    const saveInput = (e) => {
-        state[e.target.name] = e.target.value;
-        setState(state);
-    }
+export default function OverallCreate(props) {
 
     const [descriptions, setDescriptions] = useState([]);
+    const [types, setTypes] = useState([]);
+    const [options, setOptions] = useState([]);
+    const columns = props.columns.filter(column => column !== 'Medico' && column !== 'Fecha_data' && column !== 'Hora_data' && column !== 'Seguridad' && column !== 'id');
+
+    const formik = useFormik({
+        initialValues: initialValues(columns),
+        validationSchema: Yup.object( validation(columns, types) ),
+        onSubmit: (formValue) => {
+            console.log(formValue);
+        },
+    });
 
     const columnDescription = async () => {
         try {
@@ -28,9 +35,54 @@ function OverallCreate(props) {
           console.log(error);
           return error;
         }
-      };
+    };
 
-    columnDescription();
+    const columnsTypes = async () => {
+        try {
+          const { data } = await httpConToken.post(
+            "/root/procesos/maestros-matrix/tipos-columnas",
+            {
+              tableName: props.tableName,
+            }
+          );
+          setTypes(data.data);
+          
+        } catch (error) {
+          console.log(error);
+          return error;
+        }
+    };
+
+    const getOptions = async (comentarios) => {
+        try {
+          const { data } = await httpConToken.post(
+            "/root/procesos/maestros-matrix/selecciones",
+            {
+              tableName: props.tableName,
+              comentarios: comentarios
+            }
+          );
+          setOptions(data.data);
+        } catch (error) {
+          console.log(error);
+          return error;
+        }
+    };
+
+    const genderOptions = (comentarios) => {
+        return (
+            [
+                { key: 'm', text: 'Male', value: 'male' },
+                { key: 'f', text: 'Female', value: 'female' },
+                { key: 'o', text: 'Other', value: 'other' },
+              ]
+        );
+    }
+
+    useEffect( async() => {
+        await columnDescription();
+        await columnsTypes();
+    }, [props.columns])
 
     return (
         <section>
@@ -41,7 +93,7 @@ function OverallCreate(props) {
                             <h5 className="modal-title" id="exampleModalLabel">Crear registro en la tabla {props.tableName}</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <form onSubmit={() => overallCreate({props, state})}>
+                        <Form onSubmit={formik.handleSubmit}>
                             <div className="modal-body">
                                 
                                     <div className="container">
@@ -50,7 +102,55 @@ function OverallCreate(props) {
                                                 <div className="row ml-auto" key={column}>
                                                     <h1 className="col-6 label form-label">({descriptions[index]}) <br /> {column}</h1>
                                                     <div className='col-6 right-field'>
-                                                        <DinamicField column={column} tableName={props.tableName} saveInput={saveInput} />
+
+                                                        {
+                                                            {
+                                                                '0' : <Form.Input 
+                                                                        type='text'
+                                                                        name = {column}
+                                                                        value={formik.values[column]}
+                                                                        onChange={formik.handleChange}
+                                                                        error={formik.errors[column]}
+                                                                        
+                                                                    />,
+                                                                '1' :  <Form.Input 
+                                                                            type='number'
+                                                                            name = {column}
+                                                                            value={formik.values[column]}
+                                                                            onChange={formik.handleChange}
+                                                                            error={formik.errors[column]}
+                                                                        />,  
+                                                                '2' :  <Form.Input 
+                                                                            type='number'
+                                                                            name = {column}
+                                                                            value={formik.values[column]}
+                                                                            onChange={formik.handleChange}
+                                                                            error={formik.errors[column]}
+                                                                        />,  
+                                                                '3' :  <Form.Input 
+                                                                            type='text'
+                                                                            name = {column}
+                                                                            value={formik.values[column]}
+                                                                            onChange={formik.handleChange}
+                                                                            error={formik.errors[column]}
+                                                                        />,
+                                                                '5' : <Form.Field
+                                                                            control={Select}
+                                                                            options={genderOptions(types[index]?.comentarios)}
+                                                                            placeholder='Seleccione'
+                                                                            search
+                                                                            searchInput={{ id: 'form-select-control-gender-'+index }}
+                                                                            error={formik.errors[column]}
+                                                                        />
+                                                            }[types[index]?.tipo] || <Form.Input 
+                                                                                        type='text'
+                                                                                        name = {column}
+                                                                                        value={formik.values[column]}
+                                                                                        onChange = {formik.handleChange}
+                                                                                        disabled
+                                                                                    />
+                                                        }
+                                                        
                                                     </div>
                                                 </div>
                                             )) : 'Loading...'
@@ -59,9 +159,13 @@ function OverallCreate(props) {
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" className="btn btn-primary" >Save changes</button>
+                                <Button
+                                    type="submit"
+                                    className="btn-submit"
+                                >Entrar</Button>
+                                
                             </div>
-                        </form>
+                        </Form>
                     </div>
                 </div>
             </div>
@@ -69,5 +173,38 @@ function OverallCreate(props) {
     );
 }
 
-export default OverallCreate;
+
+function initialValues(columns) {
+
+    let values = new Object();
+
+    columns.map( (column) => {
+        values[column] = ""
+    });
+
+    return values;
+}
+
+function validation(columns, types) {
+    let values = new Object();
+    columns.map( (column, index) => {
+        switch(types[index]?.tipo) {
+            case '0':
+                values[column] = Yup.string("El valor debe ser un String").required("El campo debe estar lleno");
+                break;
+            case '1':
+                values[column] = Yup.number("Debe ser un numero").integer("debe ser entero").positive("debe ser positivo").required("El campo debe estar lleno");
+                break;
+            case '2':
+                values[column] = Yup.number("Debe ser un numero Real").required("El campo debe estar lleno");
+                break;
+            case '3':
+                values[column] = Yup.string().matches(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/, "Debe estar en formato YYYY-MM-DD").required("El campo debe estar lleno");
+                break;
+            default:
+                break;
+        }
+    });
+    return values;
+}
 

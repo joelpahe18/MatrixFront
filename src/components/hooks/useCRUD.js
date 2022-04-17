4ccbe570670ddb840c5a3c8fdca554dedf20d8c3
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-16 20:33:41 -0500

    control z

--- src/components/hooks/useCRUD.js
+++ src/components/hooks/useCRUD.js
@@ -23,13 +23,14 @@ const generateDynamicObject = ({ typeOperation, props, state }) => {
 
 const overallCreate = async ({ props, state }) => {
   const typeOperation = "create";
+  const overallObject = generateDynamicObject({ typeOperation, props, state });
 
   await axios
     .post(
       URL.BASE_URL +
         `/root/procesos/maestros-matrix/editar-datos-matrix/registrar/` +
         props.tableName,
-      state,
+      overallObject,
       {
         headers: { Authorization: `Bearer ${localStorage.acces_token}` },
       }
